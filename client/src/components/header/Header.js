ed796f1f25481042432fad14b33687eb2532fd01
Stuart <stupop010@hotmail.co.uk>
2020-01-24 23:52:01 +0000

    header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useStyles from './headerStyle';
 
-const Header = () => {
+const Header = ({ isAuth }) => {
   const classes = useStyles();
+
   return (
     <AppBar position="static" className={classes.header}>
       <Toolbar>
@@ -14,16 +16,34 @@ const Header = () => {
           B-Chat
         </Typography>
         <ul className={classes.flex}>
-          <Link to="/login" className={classes.linkPadding}>
-            <Typography>Login</Typography>
-          </Link>
-          <Link to="/register" className={classes.linkPadding}>
-            <Typography>Register</Typography>
-          </Link>
+          {!isAuth && (
+            <>
+              <Link to="/login" className={classes.linkPadding}>
+                <Typography>Login</Typography>
+              </Link>
+              <Link to="/register" className={classes.linkPadding}>
+                <Typography>Register</Typography>
+              </Link>
+            </>
+          )}
+          {isAuth && (
+            <>
+              <Link to="/dashboard" className={classes.linkPadding}>
+                <Typography>Dashboard</Typography>
+              </Link>
+              <Link to="/logout" className={classes.linkPadding}>
+                <Typography>Logout</Typography>
+              </Link>
+            </>
+          )}
         </ul>
       </Toolbar>
     </AppBar>
   );
+};
+
+Header.propTypes = {
+  isAuth: PropTypes.bool.isRequired,
 };
 
 export default Header;
