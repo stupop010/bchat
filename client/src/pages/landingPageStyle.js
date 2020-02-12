c17fc6d363f1236f650920bd4d8c80d20de4a804
Stuart <stupop010@hotmail.co.uk>
2020-02-12 22:02:34 +0000

    responsive landing page

--- client/src/pages/landingPageStyle.js
+++ client/src/pages/landingPageStyle.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
     width: '100%',
@@ -8,30 +8,45 @@ const useStyles = makeStyles(() => ({
     background: 'rgb(245, 245, 245)',
   },
   pic: {
-    height: '500px',
-    width: '700px',
+    width: '100%',
   },
   heroContent: {
     display: 'grid',
     gridTemplateColumns: 'repeat(2,1fr)',
     padding: '2rem',
+    [theme.breakpoints.down('sm')]: {
+      gridTemplateColumns: '1fr',
+      textAlign: 'center',
+    },
   },
   heroInfo: {
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column',
+    marginBottom: '1rem',
   },
   infoContent: {
     display: 'grid',
     gridTemplateColumns: 'repeat(3,1fr)',
     columnGap: '2rem',
     padding: '2rem',
+    [theme.breakpoints.down('md')]: {
+      gridTemplateColumns: 'repeat(2,1fr)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      gridTemplateColumns: '1fr',
+    },
   },
   card: {
     padding: '1rem',
+    marginBottom: '2rem',
   },
   title: {
-    fontSize: '4.8rem',
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2.5rem',
+    },
   },
   mt1: {
     marginTop: '1rem',
